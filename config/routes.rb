1f296cdd142215fae66f81649b2ea987cb4ead02
Hyun Hee (Clara) Kim <clara34kr@gmail.com>
2017-02-19 18:29:52 -0500

    fixed conflicts in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ KcwWorld::Application.routes.draw do
   resources :categories
   resources :proverbs
   resources :photos
-    
   get 'home' => 'home#index', :as => :home
+  resources :articles
   root :to => 'home#index'
 
 end
